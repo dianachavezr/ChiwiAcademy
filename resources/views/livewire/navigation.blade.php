c5c1920611de228ff09d859e5b3e52104a71eac4
mint <danielavargashidalgo@gmail.com>
2022-04-21 11:10:23 -0500

    logo

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-gray-800" x-data="{open: false }">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div class="absolute inset-y-0 left-0 flex items-center md:hidden">
           <!-- Mobile menu button-->
           <button x-on:click='open = true' x-on:click.away='open = false'  type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
@@ -32,7 +32,8 @@
 
             <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
           </a >
-          <a href="">  <h1 class="text-white flex justify-center mb-5  mb-5 text-3xl  text-gray-600 ">ChiwiAcademy</h1></a>
+      {{--     <a href="">  <h1 class="text-white flex justify-center mb-5  mb-5 text-3xl  text-gray-600 md:p-5">
+              ChiwiAcádemy</h1></a> --}}
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
